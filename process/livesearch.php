99e4e707041652ee18edb37ae17e9699f99dd60d
장진우 <arzz@arzz.com>
2016-09-02 18:16:07 +0900

    주석 수정

--- process/livesearch.php
+++ process/livesearch.php
@@ -1,13 +1,21 @@
 <?php
 /**
+ * 이 파일은 iModule Keyword 모듈의 일부입니다. (https://www.imodule.kr)
+ *
  * 전달된 키워드를 분리하여 분리된 키워드와 앞부분이 일치하는 자동검색어 목록을 반환합니다.
- * 
+ *
+ * @file /modules/keyword/ModuleKeyword.class.php
+ * @author Arzz (arzz@arzz.com)
+ * @license MIT License
+ * @version 3.0.0.160902
+ *
  * @post string $keyword 검색어
+ * @return object $results
  */
 if (defined('__IM__') == false) exit;
 
 $keyword = Request('keyword');
-			
+
 if ($keyword != null && strlen($keyword) > 0) {
 	$keycode = $this->getKeycode($keyword);
 	$engcode = $this->getEngcode($keycode);
